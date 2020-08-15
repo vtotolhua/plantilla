e83940e9dfb95a0e0159a86da6166547b5b19da2
vladimir totolhua <vtotolhua@gmail.com>
2020-08-14 19:27:02 -0500

    Agregamos las fuentes y los componentes de encabezado, cuerpo y piepagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
+import { PiepaginaComponent } from './componentes/piepagina/piepagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    CuerpoComponent,
+    PiepaginaComponent,
+
   ],
   imports: [
     BrowserModule
